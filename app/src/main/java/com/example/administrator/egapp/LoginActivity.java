93b75e8af7865f4cfddf313c0dc96072ccedfc4d
陈攀宇 <2965390478@qq.com>
2022-09-16 11:25:56 +0800

    第一次课，完成了登录和注册页面

---
+++ app/src/main/java/com/example/administrator/egapp/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.administrator.egapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
