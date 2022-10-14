d51462926a37b3109a1b0eab9de6f7aaf8475e03
陈攀宇 <2965390478@qq.com>
2022-10-14 09:33:26 +0800

    Rejected commit: Default

--- app/src/main/java/com/example/administrator/egapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/egapp/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.administrator.egapp;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private TextView tvDangan,tvShenghuo,tvGeren;
     private List<Fragment> fragmentList=new ArrayList<Fragment>();
+    private SlidingPaneLayout sliding_menu;
+    private ListView listViewMenu;
+//    需要解析给listView解析用
+    private String[]data={"档案管理","生活服务","个人中心"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
         tvDangan=(TextView)findViewById(R.id.tv_dangan);
         tvShenghuo=(TextView)findViewById(R.id.tv_chenghuo);
         tvGeren=(TextView)findViewById(R.id.tv_grren);
+        sliding_menu=(SlidingPaneLayout)findViewById(R.id.sliding_menu);
+        listViewMenu=(ListView)findViewById(R.id.listview_menu);
+//        定义listview的数据，使用adapter
+        ArrayAdapter<String> listAdapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
+        listViewMenu.setAdapter(listAdapter);
+        listViewMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                sliding_menu.closePane();
+                viewPager.setCurrentItem(position);
+            }
+        });
 //统一管理
         fragmentList.add(new Mainitem1());
         fragmentList.add(new Mainitem2());
